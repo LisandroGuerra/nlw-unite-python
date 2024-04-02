c2401992efe60c3e4e4aa63cc870f277fa500116
Lisandro Guerra <lisandro.digital@gmail.com>
2024-04-02 11:57:42 +0100

    feat: implements declarative base and events entity

---
+++ src/models/entities/events.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from src.models.settings.base import Base
+
+class Events(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String)
+    slug = Column(String, nullable=False)
+    maximum_attendees = Column(Integer)
+    
